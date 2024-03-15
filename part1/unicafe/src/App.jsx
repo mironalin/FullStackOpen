3fd4e20782cfe1921718edbff02fd718a502a66a
mironalin <alinmiron3@gmail.com>
2024-03-15 21:05:10 +0200

    1.7: unicafe step2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,15 +1,27 @@
 import { useState } from "react";
 
+const Heading = ({ text }) => {
+  return <h1>{text}</h1>;
+};
+
 const Button = ({ onClick, text }) => {
   return <button onClick={onClick}>{text}</button>;
 };
 
-const Display = ({ grade, value }) => {
+const Display = ({ text, value }) => {
   return (
     <p>
-      {grade} {value}
+      {text} {value}
     </p>
   );
+};
+
+const Average = ({ good, neutral, bad }) => {
+  return <p>average {(good - bad) / (good + neutral + bad)}</p>;
+};
+
+const Positive = ({ good, count }) => {
+  return <p>positive {(good / count) * 100} %</p>;
 };
 
 function App() {
@@ -19,14 +31,17 @@ function App() {
 
   return (
     <div>
-      <h1>give feedback</h1>
+      <Heading text="give feedback" />
       <Button onClick={() => setGood(good + 1)} text="good" />
       <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button onClick={() => setBad(bad + 1)} text="bad" />
-      <h1>statistics</h1>
-      <Display grade="good" value={good} />
-      <Display grade="neutral" value={neutral} />
-      <Display grade="bad" value={bad} />
+      <Heading text="statistics" />
+      <Display text="good" value={good} />
+      <Display text="neutral" value={neutral} />
+      <Display text="bad" value={bad} />
+      <Display text="all" value={good + neutral + bad} />
+      <Average good={good} neutral={neutral} bad={bad} />
+      <Positive good={good} count={good + neutral + bad} />
     </div>
   );
 }
